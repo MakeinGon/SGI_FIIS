8de5056954da4c9e91d309749b788561b558e3bf
MakeinGon <makein.gonzales@unas.edu.pe>
2026-06-11 10:34:46 -0500

    Proyecto inicial SGI FIIS

---
+++ backend/src/test/java/pe/edu/unas/fiis/sgi/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.unas.fiis.sgi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
